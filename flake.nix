8f58c708041da4d97ea76abe9888115fa93050b8
Dennis Lawter <bytomancer@gmail.com>
2025-11-07 13:15:54 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Bytomancer's NixOS configuration base flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      hvergelmir = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/hvergelmir/configuration.nix
+        ];
+      };
+    };
+  };
+}
+
