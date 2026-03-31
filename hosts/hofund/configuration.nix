429e1d4cbf413625bd8baa5a21831103e5be7d1b
Bytomancer <bytomancer@gmail.com>
2026-03-31 14:07:11 -0400

    Updated hofund

--- hosts/hofund/configuration.nix
+++ hosts/hofund/configuration.nix
@@ -17,7 +17,9 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
-  
+
+  networking.hostName = "Hofund";
+
   services.openssh.enable = true;
   
   system.stateVersion = "25.05";
